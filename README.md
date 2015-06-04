e00006a3aeac6b5c4bfa5a7902669ff26ef25e84
Oguz Bastemur <obastemur@gmail.com>
2015-06-05 00:43:59 +0200

    add octane

--- README.md
+++ README.md
@@ -1,17 +1,49 @@
-Run sunspider tests locally on [JXcore](https://github.com/jxcore/jxcore) to measure the embedded JavaScript engine performance.
+Run sunspider and octane tests locally on (alphabetical order) [IO.JS](http://iojs.org) / [JXcore](http://jxcore.com) / [Node.JS](http://nodejs.org) to measure the embedded JavaScript engine performance.
+
+#### Remarks
+
+ - Test files are standard JS engine benchmarks.
+ - Bechmarks do not measure the underlying framework's performance. So, this is not a comparison tool for i.e. JXcore vs Node etc.
+ - This implementation is designed to stress the GC / calculations / memory stability of the engine within the framework.
 
 #### How To
 
 ```
-jx runTest.js
+[node process] runTest.js
 ```
 
 #### Sample Result
 MacBook Pro i7-2.7Ghz (JXcore 0.3.0.1 SM 34 / V8 3.14) Results;  
-**SpiderMonkey**: 296.6 ms (Winner)  
-**V8**: 330.6 ms  
+**SpiderMonkey 34**
+Richards: 22106
+DeltaBlue: 25641
+Crypto: 22738
+RayTrace: 28099
+EarleyBoyer: 19687
+RegExp: 3347
+Splay: 19228
+NavierStokes: 27640
+
+** Octane Score (version 7): 18319 (Higher is faster)**
+
+** Total SunSpider AVG. Time 164.6 ms**
+
+
+**v8 3.14**
+Richards: 16355
+DeltaBlue: 20523
+Crypto: 19987
+RayTrace: 19832
+EarleyBoyer: 35854
+RegExp: 5295
+Splay: 5696
+NavierStokes: 24906
+
+** Octane Score (version 7): 15642 (Higher is faster)**
+
+** Total SunSpider AVG. Time 165.4 ms**
 
 
 #### Intention
-Measure embedded JS engine performance
+Measure embedded JS engine performance for Node.JS platform.
 
--- runTest.js
+++ runTest.js
@@ -1,54 +1,36 @@
-/*
- The MIT License (MIT)
-
- Copyright (c) 2015 Oguz Bastemur
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
-
-
 var fs = require('fs');
 var path = require('path');
 var testVersion = '1.0.1';
 var folder = path.join(process.cwd(), 'sunspider-' + testVersion);
 var files = fs.readdirSync(folder);
-var totalTime = 0;
-var counter = 0, subCounter = 1;
-var runner = setInterval(function() {
-  if (counter < files.length) {
-    var file = files[counter];
-    counter++;
+var totalTime = 0, subCounter = 1;
+var vm = require('vm');
 
-    if (path.extname(file) != '.js') return;
+{
+  var script = fs.readFileSync("./octane.js") + "";
+  var sandbox = {print: console.log};
+  var context = vm.createContext(sandbox);
+  vm.runInContext(script, context);
+  console.log("------\n");
+}
 
-    var out = jxcore.utils.cmdSync(process.argv[0] + " runSingle.js " + folder + path.sep + file);
-    if (out.exitCode) {
-      throw new Error(out.out);
-    }
-    totalTime += parseInt(out.out);
-
-  } else if (subCounter < 10) {
-    counter = 0;
-    subCounter ++;
-  } else {
-    clearInterval(runner);
-
-    console.log("Average of 10 runs: ", totalTime / 10);
+setInterval(function(){
+  if (subCounter == 10) {
+    console.log("Total SunSpider AVG. Time", totalTime/ 10);
+    process.exit(0);
   }
-}, 1);
+
+  subCounter++;
+  for(var counter = 0;counter < files.length; counter++) {
+    var file = files[counter];
+
+    if (path.extname(file) != '.js') continue;
+
+    var script = fs.readFileSync(folder + path.sep + file) + "";
+    var sandbox = {print: console.log};
+    var context = vm.createContext(sandbox);
+
+    var subTotal = vm.runInContext("var START_TIME=Date.now();" + script + ";Date.now()-START_TIME", context);
+    totalTime += subTotal;
+  }
+}, 123);
