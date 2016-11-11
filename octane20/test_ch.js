ff9fe69d8a4c519957a50049c2b489db3aa8b01b
Oguz Bastemur <ogbastem@microsoft.com>
2016-11-11 19:41:43 +0100

    add octane20 ch

---
+++ octane20/test_ch.js
@@ -0,0 +1,46 @@
+// var files = ["3d-cube.js","bitops-bits-in-byte.js","math-cordic.js",
+// "3d-morph.js","bitops-bitwise-and.js","math-partial-sums.js",
+// "3d-raytrace.js","bitops-nsieve-bits.js","math-spectral-norm.js",
+// "controlflow-recursive.js","regexp-dna.js",
+// "access-binary-trees.js","crypto-aes.js","string-base64.js",
+// "access-fannkuch.js","crypto-md5.js","string-fasta.js",
+// "access-nbody.js","crypto-sha1.js","string-tagcloud.js",
+// "access-nsieve.js","date-format-tofte.js","string-unpack-code.js",
+// "bitops-3bit-bits-in-byte.js","date-format-xparb.js","string-validate-input.js"];
+
+var base_dir = '../octane/';
+WScript.LoadScriptFile(base_dir + 'base.js');
+WScript.LoadScriptFile(base_dir + 'richards.js');
+WScript.LoadScriptFile(base_dir + 'deltablue.js');
+WScript.LoadScriptFile(base_dir + 'crypto.js');
+WScript.LoadScriptFile(base_dir + 'raytrace.js');
+WScript.LoadScriptFile(base_dir + 'earley-boyer.js');
+WScript.LoadScriptFile(base_dir + 'regexp.js');
+WScript.LoadScriptFile(base_dir + 'splay.js');
+WScript.LoadScriptFile(base_dir + 'navier-stokes.js');
+WScript.LoadScriptFile(base_dir + 'pdfjs.js');
+WScript.LoadScriptFile(base_dir + 'mandreel.js');
+WScript.LoadScriptFile(base_dir + 'gbemu-part1.js');
+WScript.LoadScriptFile(base_dir + 'gbemu-part2.js');
+WScript.LoadScriptFile(base_dir + 'code-load.js');
+WScript.LoadScriptFile(base_dir + 'box2d.js');
+WScript.LoadScriptFile(base_dir + 'zlib.js');
+WScript.LoadScriptFile(base_dir + 'zlib-data.js');
+WScript.LoadScriptFile(base_dir + 'typescript.js');
+WScript.LoadScriptFile(base_dir + 'typescript-input.js');
+WScript.LoadScriptFile(base_dir + 'typescript-compiler.js');
+WScript.LoadScriptFile(base_dir + 'run.js');
+// var files = [
+// "octane/base.js",
+// "octane/box2d.js","octane/mandreel.js","octane/splay.js",
+// "octane/code-load.js","octane/navier-stokes.js","octane/typescript-compiler.js",
+// "octane/crypto.js","octane/pdfjs.js","octane/typescript-input.js",
+// "octane/deltablue.js","octane/raytrace.js","octane/typescript.js",
+// "octane/earley-boyer.js","octane/regexp.js","octane/zlib-data.js",
+// "octane/gbemu-part1.js","octane/gbemu-part2.js","octane/richards.js","octane/zlib.js","octane/run.js",
+// ];
+//
+// for(var o in files) {
+//   print("::" + files[o]);
+//   WScript.LoadScriptFile("../" + files[o], "self");
+// }
